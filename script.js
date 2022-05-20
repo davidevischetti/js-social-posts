ea4d1641780d98ea83c2bd831129be1aae359c40
Davide <davidevischetti@gmail.com>
2022-05-20 15:52:57 +0200

    struttura html completa

---
+++ script.js
@@ -0,0 +1,44 @@
+// const myPost = document.createElement('div');
+// myPost.className = 'post';
+
+// const myPostHeader = document.createElement('div');
+// myPostHeader.className = 'post__header';
+
+const myContainer = document.getElementById('container');
+
+posts.forEach(element => {
+    myContainer.innerHTML += 
+    `<div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src=${element.author.image} alt="Phil Mangione">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${element.author.name}</div>
+                <div class="post-meta__time">${element.created}</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">${element.content}</div>
+    <div class="post__image">
+        <img src="${element.media}" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+            </div>
+        </div> 
+    </div>            
+    </div>`
+});
+
+
+
