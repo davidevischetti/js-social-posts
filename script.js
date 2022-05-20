adba756c845c320c4eddaa60b87495c65f5bc72b
Davide <davidevischetti@gmail.com>
2022-05-20 17:02:21 +0200

    tentativo aumento likes

--- script.js
+++ script.js
@@ -1,44 +1,67 @@
-// const myPost = document.createElement('div');
-// myPost.className = 'post';
 
-// const myPostHeader = document.createElement('div');
-// myPostHeader.className = 'post__header';
-
+// VARIABILI DI RICHIAMO HTML
 const myContainer = document.getElementById('container');
 
 posts.forEach(element => {
-    myContainer.innerHTML += 
+
+    // RICHIAMO LA FUNZIONE CREAZIONE HTML
+    createHtmlFunc(element);
+    
+    let myBtn = document.querySelector(`.js-like-button${element.id}`);
+    let myLikes = document.getElementById(`like-counter-${element.id}`);
+
+    console.log(element.likes);
+    
+    
+    myBtn.addEventListener('click', function () {
+        element.likes ++;
+        myLikes.innerHTML = element.likes;
+        console.log(element.likes);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+// FUNZIONE CREAZIONE L'HTML
+function createHtmlFunc (obj) {
+
+
+    myContainer.innerHTML +=
     `<div class="post">
     <div class="post__header">
         <div class="post-meta">                    
             <div class="post-meta__icon">
-                <img class="profile-pic" src=${element.author.image} alt="Phil Mangione">                    
+                <img class="profile-pic" src=${obj.author.image} alt="${obj.author.name}>                    
             </div>
             <div class="post-meta__data">
-                <div class="post-meta__author">${element.author.name}</div>
-                <div class="post-meta__time">${element.created}</div>
+                <div class="post-meta__author">${obj.author.name}</div>
+                <div class="post-meta__time">${obj.created}</div>
             </div>                    
         </div>
     </div>
-    <div class="post__text">${element.content}</div>
+    <div class="post__text">${obj.content}</div>
     <div class="post__image">
-        <img src="${element.media}" alt="">
+        <img src="${obj.media}" alt="">
     </div>
     <div class="post__footer">
         <div class="likes js-likes">
             <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="1">
+                <a class="like-button  js-like-button${obj.id}" href="#like-counter-${obj.id}" data-postid="${obj.id}">
                     <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                     <span class="like-button__label">Mi Piace</span>
                 </a>
             </div>
             <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+                Piace a <b id="like-counter-${obj.id}" class="js-likes-counter">${obj.likes}</b> persone
             </div>
         </div> 
     </div>            
-    </div>`
-});
-
-
-
+    </div>`;
+};
